42bb256ef5dafeef056ea80693d724dcd0e0d998
lsb567 <1092244357@qq.com>
2022-04-01 21:56:29 +0800

    1

---
+++ object4/demo2.py
@@ -0,0 +1,6 @@
+qq = input('请输入qq号：')
+pwd = input('请输入密码：')
+if qq == '1092244357' and pwd == '123':
+    print('登录成功')
+else:
+    print('输入不正确！')
---
+++ object4/demo3.py
@@ -0,0 +1,14 @@
+import random
+
+price = random.randint(1000, 1500)
+print('今日竞猜的商品为小米扫地机器人：价格在[1000 - 1500]之间：')
+guess = int(input())
+
+if guess > price:
+    print('大了')
+elif guess < price:
+    print('小了')
+else:
+    print('猜对了')
+
+print('真实价格为：', price)
